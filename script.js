ea80d63d7417c29eaa3c77f57eafd3cd95da0024
Jamshidbek <kamoliddinovjamshid7@gmail.com>
2024-01-27 23:54:20 +0500

    dark/light mode toggle button added

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+//! Getting Elements from HTML
 let settingsMenu = document.querySelector('.settings-menu');
+let darkBtn = document.querySelector('#dark-btn');
+
+
 
 //! Settings Menu Toggle Function
-
 function settingsMenuToggle() {
     settingsMenu.classList.toggle('settings-menu-height');
 }
+
+//! Dark/Light Mode Toggle Function
+darkBtn.onclick = function() {
+    darkBtn.classList.toggle('dark-btn-on');
+}
+
