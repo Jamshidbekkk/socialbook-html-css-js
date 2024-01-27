767d5aaf648f7bf717e974d6da277f67e99abbe2
Jamshidbek <kamoliddinovjamshid7@gmail.com>
2024-01-27 23:38:57 +0500

    main content html and css parts done

---
+++ script.js
@@ -0,0 +1,7 @@
+let settingsMenu = document.querySelector('.settings-menu');
+
+//! Settings Menu Toggle Function
+
+function settingsMenuToggle() {
+    settingsMenu.classList.toggle('settings-menu-height');
+}
